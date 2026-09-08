eefbcbe2673198bfebca48e5a7c7103f7e4e936d
2622010Onoe <hmj2622010@stu.o-hara.ac.jp>
2026-09-08 09:47:52 +0900

    正しく位置を反映できるように変更

--- main.cpp
+++ main.cpp
@@ -6,10 +6,14 @@ const int WIDTH = 1200, HEIGHT = 720;	// ウィンドウの幅と高さのピクセル数
 const int FPS = 60;
 const int PLAYER_POWER_MAX = 100;
 
-int imgHand, imgSword;
+// 武器の種類(全部使うかは不明)
+int imgSword, imgScythe, imgSpear, imgAxe, imgKatana, imgMightyHammer,imgPickaxe,imgArrow,imgBayonet,imgChainsaw;
+int imgCutterBlade, imgCyberSword, imgDarkSword, imgDragonKeyring, imgFireSword, imgKey, imgKitchenKnife, imgLaserSaber, imgMagicHand, imgNaginata;
+int imgPencil, imgPixelSword, imgRapier, imgScissors, imgScrewdriver, imgShinai, imgShovel, imgSpatula, imgToySword, imgUmbrella;
 
 int imgGround,imgTable, imgSky;
-
+int weaponAngle = 0;
+int weaponOut = 0;
 int power;
 int rightSwordRange = 2;
 int leftSwordRange = 2;
@@ -26,15 +30,18 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 
 	while(1)
 	{
-		//DrawImageEnlarge(imgSky, WIDTH / 2, HEIGHT / 1.4, 30, 20);
+		ClearDrawScreen();
+		DrawImageEnlarge(imgSky, WIDTH / 2, HEIGHT / 1.4, 30, 20);
+		DrawImageRotateEnlarge(imgSword, WIDTH/2, HEIGHT*1.5+(weaponOut/80.0f),15.0f,weaponAngle/10.0f);
+		DrawImageEnlarge(imgGround, WIDTH / 2, HEIGHT / 1.5, 30, 30);
+		//DrawImageEnlarge(imgTable, WIDTH / 2, HEIGHT / 1.3, 10, 10);
+		if (CheckHitKey(KEY_INPUT_RIGHT) == 1)weaponAngle++;
+		if (CheckHitKey(KEY_INPUT_LEFT) == 1)weaponAngle--;
+		if (CheckHitKey(KEY_INPUT_UP) == 1)weaponOut--;
+		if (CheckHitKey(KEY_INPUT_DOWN) == 1)weaponOut++;
 
-		//DrawImageEnlarge(imgHand, WIDTH / 2, HEIGHT / 1.3, 10, 10);
-		//DrawImageEnlarge(imgGround, WIDTH / 2, HEIGHT / 1.5, 30, 30);
-		DrawImageEnlarge(imgTable, WIDTH / 2, HEIGHT / 1.3, 10, 10);
-
-		DrawImageRotateEnlarge(imgSword, 0, 0,10.0f,0);
 		ScreenFlip();	// 裏画面の内容を表画面に反映させる
-		WaitKey();
+		//WaitKey();
 		WaitTimer(1000 / FPS);	// 一定時間待つ
 		if (ProcessMessage() == -1)break;	// Windowsから情報を受け取りエラーが起きたら終了
 		if (CheckHitKey(KEY_INPUT_ESCAPE) == 1)break;	// ESCキーが押されたら終了
@@ -46,8 +53,38 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 // 初期化用の関数
 void InitGame(void)
 {
-	imgHand = LoadGraphWithCheck("image/Nuku_Hand.png");
-	imgSword = LoadGraphWithCheck("image/Nuku_Sword.png");
+	imgSword = LoadGraphWithCheck("image/Nuku_SwordSize.png");
+	imgScythe = LoadGraphWithCheck("image/Nuku_Scythe.png");
+	imgSpear = LoadGraphWithCheck("image/Nuku_Spear.png");
+	imgAxe = LoadGraphWithCheck("image/Nuku_Axe.png");
+	imgKatana = LoadGraphWithCheck("image/Nuku_Katana.png");
+	imgMightyHammer = LoadGraphWithCheck("image/Nuku_MightyHammer.png");
+	imgPickaxe = LoadGraphWithCheck("image/Nuku_Pickaxe.png");
+	imgArrow = LoadGraphWithCheck("image/Nuku_Arrow.png");
+	imgBayonet = LoadGraphWithCheck("image/Nuku_Bayonet.png");
+	imgChainsaw = LoadGraphWithCheck("image/Nuku_Chainsaw.png");
+	imgCutterBlade = LoadGraphWithCheck("image/Nuku_CutterBlade.png");
+	imgCyberSword = LoadGraphWithCheck("image/Nuku_CyberSword.png");
+	imgDarkSword = LoadGraphWithCheck("image/Nuku_DarkSword.png");
+	imgDragonKeyring = LoadGraphWithCheck("image/Nuku_DragonKeyring.png");
+	imgFireSword = LoadGraphWithCheck("image/Nuku_FireSword.png");
+	imgKey = LoadGraphWithCheck("image/Nuku_Key.png");
+	imgKitchenKnife = LoadGraphWithCheck("image/Nuku_KitchenKnife.png");
+	imgLaserSaber = LoadGraphWithCheck("image/Nuku_LaserSaber.png");
+	imgMagicHand = LoadGraphWithCheck("image/Nuku_MagicHand.png");
+	imgNaginata = LoadGraphWithCheck("image/Nuku_Naginata.png"); 
+	imgPencil = LoadGraphWithCheck("image/Nuku_Pencil.png");
+	imgPixelSword = LoadGraphWithCheck("image/Nuku_PixelSword.png");
+	imgRapier = LoadGraphWithCheck("image/Nuku_Rapier.png");
+	imgScissors = LoadGraphWithCheck("image/Nuku_Scissors.png");
+	imgScrewdriver = LoadGraphWithCheck("image/Nuku_Screwdriver.png");
+	imgShinai = LoadGraphWithCheck("image/Nuku_Shinai.png");
+	imgShovel = LoadGraphWithCheck("image/Nuku_Shovel.png");
+	imgSpatula = LoadGraphWithCheck("image/Nuku_Spatula.png");
+	imgToySword = LoadGraphWithCheck("image/Nuku_ToySword.png");
+	imgUmbrella = LoadGraphWithCheck("image/Nuku_Umbrella.png");
+
+
 
 	imgTable = LoadGraphWithCheck("image/Nuku_Table.png");
 	imgGround = LoadGraphWithCheck("image/Nuku_Ground.png");
@@ -62,13 +99,12 @@ void DrawImageEnlarge(int img, int x, int y, int enlargeX, int enlargeY)
 	DrawExtendGraph(x - (w / 2) * enlargeX, y - (h / 2) * enlargeY, x + (w / 2) * enlargeX, y + (h / 2) * enlargeY, img, true);
 }
 
-// 角度、中心座標と大きさを指定して画像を表示する関数
+// 角度、中心座標と大きさを指定して画像を表示する関数 ←こいつと格闘中！！
 void DrawImageRotateEnlarge(int img, int x, int y, double ExtRate,int angle)
 {
 	int w, h;
 	GetGraphSize(img, &w, &h);
-	//DrawRotaGraph2(x - (w / 2), y - (h / 2), x, y,ExtRate,(angle*DX_PI_F/180.0f), img, true);
-	DrawRotaGraph2(x , y, x, y,ExtRate,(angle*DX_PI_F/180.0f), img, true);
+	DrawRotaGraph2(x - (w / 2),y - (h/ 2), w/2 , h-(weaponOut/20) ,ExtRate,(angle * DX_PI_F/180.0f), img, true);
 }
 
 int LoadGraphWithCheck(const char* file)
